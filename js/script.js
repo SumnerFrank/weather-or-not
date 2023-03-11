bca40419dd22585e4bac1ab4f19e1f06bc9e8a8e
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-03-11 08:16:51 -0500

    added fetch request for cities

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+// Universal Variables
 const apiKey = "fdb3fbd4a502e98a93742bb761dbcb16";
 const apiURL = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
 const cities = [];
+
+
+// Necessary Functions
+function getCity(city) {
+  fetch(apiURL)
+  .then(function(response) {
+    response.json().then(function(data) {
+      displayForecast(data)
+    })
+  })
+}
