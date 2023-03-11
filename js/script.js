c52d91bedee3262e2fd31cea98c4197ff6dbb63e
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-03-11 13:59:32 -0500

    copied displayWeather function into a 5 day version, adjusting syntax and logic to fit needs

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const cityInputlEl = document.querySelector('#city')
 const searchHistoryBtn = document.querySelector('#past-search-btn');
 const citySearchInputEl = document.querySelector('#searched-city')
 const weatherContainerEl = document.querySelector('#current-weather-container');
-const forecastTitleEl = document.querySelector('#forecast')
+const forecastTitle = document.querySelector('#forecast')
 const forecastContainerEl = document.querySelector('#five-day-container');
 
 
@@ -45,12 +45,12 @@ function getCityWeather(city) {
   fetch(apiURL)
   .then(function(response) {
     response.json().then(function(data) {
-      displayForecast(data, city)
+      displayWeather(data, city)
     })
   })
 }
 
-function displayForecast(weather, searchCity) {
+function displayWeather(weather, searchCity) {
   // resets to empty form 
   weatherContainerEl.textContent = ''
   citySearchInputEl.textContent = searchCity
@@ -86,6 +86,52 @@ function displayForecast(weather, searchCity) {
   weatherContainerEl.appendChild(windSpeedEl)
 
 
+}
+
+function displayForecast(weather) {
+  // resets to empty form 
+  forecastContainerEl.textContent = ''
+  forecastTitle.textContent = '5 Day Forecast:'
+
+  let forecast = weather.list 
+    for(var i=5; i < forecast.length; i=i+8) {
+      let dailyForecast = forecast[i]
+
+      const forecastEl = document.createElement('div')
+      forecastEl.classList = "card bg-primary text-light m-2"
+      
+  //date 
+  const forecastDate = document.createElement('h4')
+  forecastDate.textContent=" (" + moment(weather.dt.value).format("MMM D, YYYY") + ") "
+  forecastDate.classList = 'card-header text-center'
+  forecastEl.appendChild(forecastDate)
+
+  //image
+  var weatherIcon = document.createElement("img")
+  weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`)
+  citySearchInputEl.appendChild(weatherIcon)
+
+  //temp 
+  var temperatureEl = document.createElement("span")
+  temperatureEl.textContent = "Temperature: " + weather.main.temp + " °F"
+  temperatureEl.classList = "list-group-item"
+
+  // wind
+  var windSpeedEl = document.createElement("span")
+  windSpeedEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH"
+  windSpeedEl.classList = "list-group-item"
+
+  // humidity
+  var humidityEl = document.createElement("span")
+  humidityEl.textContent = "Humidity: " + weather.main.humidity + " %"
+  humidityEl.classList = "list-group-item"
+
+  // adds elements to container 
+  weatherContainerEl.appendChild(temperatureEl)
+  weatherContainerEl.appendChild(humidityEl)
+  weatherContainerEl.appendChild(windSpeedEl)
+
+    }
 }
 
 // function searchHistory(searchHistory) {
