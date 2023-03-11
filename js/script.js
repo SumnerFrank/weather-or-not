5c6a460a4f4a271c33d706425f6dd598a17a5a1f
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-03-11 09:48:53 -0500

    added js, created functions for forecast display

--- js/script.js
+++ js/script.js
@@ -1,14 +1,37 @@
 // Universal Variables
-const apiKey = "fdb3fbd4a502e98a93742bb761dbcb16";
+const apiKey = 'fdb3fbd4a502e98a93742bb761dbcb16';
 const apiURL = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
 const cities = [];
 const searchBtn = document.querySelector('#city-search-form');
+const cityInpulEl = document.querySelector('#city')
 const searchHistoryBtn = document.querySelector('#past-search-btn');
+const citySearchInputEl = document.querySelector('#searched-city')
 const weatherContainerEl = document.querySelector('#current-weather-container');
+const forecastTitleEl = document.querySelector('#forecast')
+const forecastContainerEl = document.querySelector('#five-day-container');
 
 
 
 // Necessary Functions
+function formSubmitHandler(event){
+  event.preventDefault()
+  var city = cityInputEl.value.trim()
+  if(city){
+      getCityWeather(city)
+      getCity(city)
+      cities.unshift({city})
+      cityInputEl.value = ""
+  } else{
+      alert("Please enter a City")
+  }
+  saveSearch()
+  pastSearch(city)
+}
+
+function saveSearch(){
+  localStorage.setItem('cities', JSON.stringify(cities))
+}
+
 function getCity(city) {
   fetch(apiURL)
   .then(function(response) {
@@ -27,9 +50,24 @@ function getCityWeather(city) {
   })
 }
 
-// function displayForecast(weather) {
+function displayForecast(weather, searchCity) {
+  weatherContainerEl.textContent = ''
+  citySearchInputEl.textContent = searchCity
 
-// }
+  //date 
+
+  //image
+
+  //temp 
+
+  // wind
+
+  // humidity
+
+
+  let lat = weather.coord.lat;
+  let lon = weather.coord.lon;
+}
 
 // function searchHistory(searchHistory) {
 
