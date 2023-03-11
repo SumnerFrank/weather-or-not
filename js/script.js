1f2d743fb023b8e2fc8a79cdd94d8845c23bee22
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-03-11 15:18:41 -0500

    syntax error resolved

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 // Universal Variables
-const apiKey = 'fdb3fbd4a502e98a93742bb761dbcb16';
-const apiURL = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
 const cities = [];
 const searchBtn = document.querySelector('#city-search-form');
 const cityInputlEl = document.querySelector('#city')
@@ -14,6 +12,7 @@ const forecastContainerEl = document.querySelector('#five-day-container');
 
 // Necessary Functions
 function formSubmitHandler(event){
+  console.log('hello world');
   event.preventDefault()
   var city = cityInputEl.value.trim()
   if(city){
@@ -25,7 +24,7 @@ function formSubmitHandler(event){
       alert("Please enter a City")
   }
   saveSearch()
-  pastSearch(city)
+  searchHistory(city)
 }
 
 function saveSearch(){
@@ -33,6 +32,8 @@ function saveSearch(){
 }
 
 function getCity(city) {
+const apiKey = 'fdb3fbd4a502e98a93742bb761dbcb16';
+const apiURL = `https://api.openweathermap.org/data/forecast?q=${city}&units=imperial&appid=${apiKey}`
   fetch(apiURL)
   .then(function(response) {
     response.json().then(function(data) {
@@ -85,6 +86,9 @@ function displayWeather(weather, searchCity) {
   weatherContainerEl.appendChild(humidityEl)
   weatherContainerEl.appendChild(windSpeedEl)
 
+let lat = weather.coord.lat;
+let lon = weather.coord.lon;
+getIndex(lat, lon)
 
 }
 
@@ -102,41 +106,47 @@ function displayForecast(weather) {
       
   //date 
   const forecastDate = document.createElement('h4')
-  forecastDate.textContent=" (" + moment(weather.dt.value).format("MMM D, YYYY") + ") "
+  forecastDate.textContent=" (" + moment(dailyForecast.dt.value).format("MMM D, YYYY") + ") "
   forecastDate.classList = 'card-header text-center'
   forecastEl.appendChild(forecastDate)
 
   //image
   var weatherIcon = document.createElement("img")
+  weatherIcon.classList = 'card-body text-center'
   weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`)
-  citySearchInputEl.appendChild(weatherIcon)
+  forecastEl.appendChild(weatherIcon)
 
   //temp 
-  var temperatureEl = document.createElement("span")
+  var forecastTemp = document.createElement("span")
   temperatureEl.textContent = "Temperature: " + weather.main.temp + " °F"
   temperatureEl.classList = "list-group-item"
 
   // wind
-  var windSpeedEl = document.createElement("span")
+  var forecastWind = document.createElement("span")
   windSpeedEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH"
   windSpeedEl.classList = "list-group-item"
 
   // humidity
-  var humidityEl = document.createElement("span")
+  var forecastHumidity = document.createElement("span")
   humidityEl.textContent = "Humidity: " + weather.main.humidity + " %"
   humidityEl.classList = "list-group-item"
 
   // adds elements to container 
-  weatherContainerEl.appendChild(temperatureEl)
-  weatherContainerEl.appendChild(humidityEl)
-  weatherContainerEl.appendChild(windSpeedEl)
+  forecastContainerEl.appendChild(forecastTemp)
+  forecastContainerEl.appendChild(forecastHumidity)
+  forecastContainerEl.appendChild(forecastWind)
 
     }
 }
 
-// function searchHistory(searchHistory) {
-
-// }
+function searchHistory(searchHistory) {
+  searchHistoryEl = document.createElement("button");
+  searchHistoryEl.textContent = searchHistory
+  searchHistoryEl.classList = 'd-flex w100 btn-light border p-2'
+  searchHistoryEl.setAttribute('data-city', searchHistory)
+  searchHistoryEl.setAttribute('type', 'submit')
+  searchHistoryEl.prepend(searchHistory)
+}
 
 // HANDLERS
 searchBtn.addEventListener("submit", formSubmitHandler);
