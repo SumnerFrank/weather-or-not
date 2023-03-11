ba3a4e4372f702c2db665eee30177950f39405bd
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-03-11 16:03:00 -0500

    refactor

--- js/script.js
+++ js/script.js
@@ -1,18 +1,14 @@
-// Universal Variables
 const cities = [];
 const searchBtn = document.querySelector('#city-search-form');
-const cityInputlEl = document.querySelector('#city')
+const cityInputEl = document.querySelector('#city')
 const searchHistoryBtn = document.querySelector('#past-search-btn');
 const citySearchInputEl = document.querySelector('#searched-city')
 const weatherContainerEl = document.querySelector('#current-weather-container');
 const forecastTitle = document.querySelector('#forecast')
 const forecastContainerEl = document.querySelector('#five-day-container');
 
-
-
 // Necessary Functions
 function formSubmitHandler(event){
-  console.log('hello world');
   event.preventDefault()
   var city = cityInputEl.value.trim()
   if(city){
@@ -28,22 +24,11 @@ function formSubmitHandler(event){
 }
 
 function saveSearch(){
-  localStorage.setItem('cities', JSON.stringify(cities))
-}
-
-function getCity(city) {
-const apiKey = 'fdb3fbd4a502e98a93742bb761dbcb16';
-const apiURL = `https://api.openweathermap.org/data/forecast?q=${city}&units=imperial&appid=${apiKey}`
-  fetch(apiURL)
-  .then(function(response) {
-    response.json().then(function(data) {
-      displayForecast(data)
-    })
-  })
+  localStorage.setItem("cities", JSON.stringify(cities))
 }
 
 function getCityWeather(city) {
-  const apiKey = 'fdb3fbd4a502e98a93742bb761dbcb16';
+  const apiKey = "df4a6db3696fc1bf82eecc368539e7a3";
   const apiURL = `https://api.openweathermap.org/data/forecast?q=${city}&units=imperial&appid=${apiKey}`
   fetch(apiURL)
   .then(function(response) {
@@ -53,68 +38,39 @@ function getCityWeather(city) {
   })
 }
 
-function displayWeather(weather, searchCity) {
-  // resets to empty form 
-  weatherContainerEl.textContent = ''
-  citySearchInputEl.textContent = searchCity
-
-  //date 
-  const currentDate = document.createElement('span')
-  currentDate.textContent=" (" + moment(weather.dt.value).format("MMM D, YYYY") + ") "
-  citySearchInputEl.appendChild(currentDate)
-
-  //image
-  var weatherIcon = document.createElement("img")
-  weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`)
-  citySearchInputEl.appendChild(weatherIcon)
-
-  //temp 
-  var temperatureEl = document.createElement("span")
-  temperatureEl.textContent = "Temperature: " + weather.main.temp + " °F"
-  temperatureEl.classList = "list-group-item"
-
-  // wind
-  var windSpeedEl = document.createElement("span")
-  windSpeedEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH"
-  windSpeedEl.classList = "list-group-item"
-
-  // humidity
-  var humidityEl = document.createElement("span")
-  humidityEl.textContent = "Humidity: " + weather.main.humidity + " %"
-  humidityEl.classList = "list-group-item"
-
-  // adds elements to container 
-  weatherContainerEl.appendChild(temperatureEl)
-  weatherContainerEl.appendChild(humidityEl)
-  weatherContainerEl.appendChild(windSpeedEl)
-
-let lat = weather.coord.lat;
-let lon = weather.coord.lon;
-getIndex(lat, lon)
-
+function getCity(city) {
+const apiKey = "df4a6db3696fc1bf82eecc368539e7a3";
+const apiURL = `https://api.openweathermap.org/data/forecast?q=${city}&units=imperial&appid=${apiKey}`
+  fetch(apiURL)
+  .then(function(response) {
+    response.json().then(function(data) {
+      displayForecast(data)
+    })
+  })
 }
+
 
 function displayForecast(weather) {
   // resets to empty form 
-  forecastContainerEl.textContent = ''
-  forecastTitle.textContent = '5 Day Forecast:'
+  forecastContainerEl.textContent = ""
+  forecastTitle.textContent = "5 Day Forecast:"
 
   let forecast = weather.list 
     for(var i=5; i < forecast.length; i=i+8) {
       let dailyForecast = forecast[i]
 
-      const forecastEl = document.createElement('div')
+      const forecastEl = document.createElement("div")
       forecastEl.classList = "card bg-primary text-light m-2"
       
   //date 
-  const forecastDate = document.createElement('h4')
-  forecastDate.textContent=" (" + moment(dailyForecast.dt.value).format("MMM D, YYYY") + ") "
-  forecastDate.classList = 'card-header text-center'
+  const forecastDate = document.createElement("h4")
+  forecastDate.textContent = moment.unix(dailyForecast.dt).format("MMM D, YYYY")
+  forecastDate.classList = "card-header text-center"
   forecastEl.appendChild(forecastDate)
 
   //image
   var weatherIcon = document.createElement("img")
-  weatherIcon.classList = 'card-body text-center'
+  weatherIcon.classList = "card-body text-center"
   weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`)
   forecastEl.appendChild(weatherIcon)
 
@@ -144,14 +100,14 @@ function displayForecast(weather) {
 function searchHistory(searchHistory) {
   searchHistoryEl = document.createElement("button");
   searchHistoryEl.textContent = searchHistory
-  searchHistoryEl.classList = 'd-flex w100 btn-light border p-2'
+  searchHistoryEl.classList = "d-flex w100 btn-light border p-2"
   searchHistoryEl.setAttribute('data-city', searchHistory)
-  searchHistoryEl.setAttribute('type', 'submit')
+  searchHistoryEl.setAttribute("type", "submit")
   searchHistoryEl.prepend(searchHistory)
 }
 
 function searchHistoryHandler(event) {
-  let city = event.target.getAttribute('data-city')
+  let city = event.target.getAttribute("data-city")
   if(city) {
     getCityWeather(city)
     getCity(city)
