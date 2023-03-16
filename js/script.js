f4dc7573f337f7a50b8a9884d4d4f4e251715736
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-03-16 08:19:14 -0400

    fixed syntax errors

--- js/script.js
+++ js/script.js
@@ -1,102 +1,101 @@
 const cities = [];
 const searchFormEl = document.querySelector('#city-search-form');
-const cityInputEl = document.querySelector('#city')
-const searchHistoryBtn = document.querySelector('#past-search-btn');
-const citySearchInputEl = document.querySelector('#searched-city')
+const cityInputEl = document.querySelector('#city');
+const searchHistoryBtn = document.querySelector('#past-search-buttons');
+const citySearchInputEl = document.querySelector('#searched-city');
 const weatherContainerEl = document.querySelector('#current-weather-container');
-const forecastTitle = document.querySelector('#forecast')
+const forecastTitle = document.querySelector('#forecast');
 const forecastContainerEl = document.querySelector('#five-day-container');
 
-// Necessary Functions
+// Necessary Functions 
 function formSubmitHandler(event){
-  event.preventDefault()
-  var city = cityInputEl.value.trim()
+  event.preventDefault();
+  var city = cityInputEl.value.trim();
   if(city){
-      getCityWeather(city)
-      getCity(city)
-      cities.unshift({city})
-      cityInputEl.value = ""
-  } else{
-      alert("Please enter a City")
+    getCityWeather(city);
+    getCity(city);
+    cities.unshift({city});
+    cityInputEl.value = "";
+  } else {
+    alert("Please enter a City");
   }
-  saveSearch()
-  searchHistory(city)
+  saveSearch();
+  searchHistory(city);
 }
 
 function saveSearch(){
-  localStorage.setItem("cities", JSON.stringify(cities))
+  localStorage.setItem("cities", JSON.stringify(cities));
 }
 
 function getCityWeather(city) {
   const apiKey = "df4a6db3696fc1bf82eecc368539e7a3";
-  const apiURL = `https://api.openweathermap.org/data/forecast?q=${city}&units=imperial&appid=${apiKey}`
+  const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
   fetch(apiURL)
-  .then(function(response) {
-    response.json().then(function(data) {
-      displayWeather(data, city)
-    })
-  })
+    .then(function(response) {
+      response.json().then(function(data) {
+        displayWeather(data, city);
+      });
+    });
 }
 
 function getCity(city) {
-const apiKey = "df4a6db3696fc1bf82eecc368539e7a3";
-const apiURL = `https://api.openweathermap.org/data/forecast?q=${city}&units=imperial&appid=${apiKey}`
+  const apiKey = "df4a6db3696fc1bf82eecc368539e7a3";
+  const apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`;
   fetch(apiURL)
-  .then(function(response) {
-    response.json().then(function(data) {
-      displayForecast(data)
-    })
-  })
+    .then(function(response) {
+      response.json().then(function(data) {
+        displayForecast(data);
+      });
+    });
 }
 
 function displayWeather(weather, citySearch) {
-  console.log('hello world')
-  weatherContainerEl.textContent = ""
-  citySearchInputEl.textContent=citySearch
+  console.log('hello world');
+  weatherContainerEl.textContent = "";
+  citySearchInputEl.textContent = citySearch;
 
   //date 
-  const currentDate = document.createElement("h4")
-  currentDate.textContent=" (" + moment(weather.dt.value).format("MMM D, YYYY") + ") "
-  citySearchInputEl.appendChild(currentDate)
+  const currentDate = document.createElement("h4");
+  currentDate.textContent = " (" + moment(weather.dt.value).format("MMM D, YYYY") + ") ";
+  citySearchInputEl.appendChild(currentDate);
 
   //image
-  var weatherIcon = document.createElement("img")
-  weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`)
-  citySearchInputEl.appendChild(weatherIcon)
+  var weatherIcon = document.createElement("img");
+  weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`);
+  citySearchInputEl.appendChild(weatherIcon);
 
   //temp 
-  var temperatureEl = document.createElement("span")
-  temperatureEl.textContent = "Temperature: " + weather.main.temp + " °F"
-  temperatureEl.classList = "list-group-item"
+  var temperatureEl = document.createElement("span");
+  temperatureEl.textContent = "Temperature: " + weather.main.temp + " °F";
+  temperatureEl.classList = "list-group-item";
 
   // wind
-  var windSpeedEl = document.createElement("span")
-  windSpeedEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH"
-  windSpeedEl.classList = "list-group-item"
+  var windSpeedEl = document.createElement("span");
+  windSpeedEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
+  windSpeedEl.classList = "list-group-item";
 
   // humidity
-  var humidityEl = document.createElement("span")
-  humidityEl.textContent = "Humidity: " + weather.main.humidity + " %"
-  humidityEl.classList = "list-group-item"
+  var humidityEl = document.createElement("span");
+  humidityEl.textContent = "Humidity: " + weather.main.humidity + " %";
+  humidityEl.classList = "list-group-item";
 
-  weatherContainerEl.appendChild(temperatureEl)
-  weatherContainerEl.appendChild(humidityEl)
-  weatherContainerEl.appendChild(windSpeedEl)
+  weatherContainerEl.appendChild(temperatureEl);
+  weatherContainerEl.appendChild(humidityEl);
+  weatherContainerEl.appendChild(windSpeedEl);
 
-
-  var lat = weather.coord.lat
-  var lon = weather.coord.lon
-  getUvIndex(lat,lon)
+  var lat = weather.coord.lat;
+  var lon = weather.coord.lon;
+  getUvIndex(lat,lon);
 }
 
 function displayForecast(weather) {
   // resets to empty form 
-  forecastContainerEl.textContent = ""
-  forecastTitle.textContent = "5 Day Forecast:"
+  forecastContainerEl.textContent = "";
+  forecastTitle.textContent = "5 Day Forecast:";
 
-  var forecast = weather.list 
-    for(var i=5; i < forecast.length; i=i+8) {
-      var dailyForecast = forecast[i]
+  var forecast = weather.list;
+  for(var i = 5; i < forecast.length; i = i + 8) {
+    var dailyForecast = forecast
 
       const forecastEl = document.createElement("div")
       forecastEl.classList = "card bg-primary text-light m-2"
@@ -110,23 +109,25 @@ function displayForecast(weather) {
   //image
   var weatherIcon = document.createElement("img")
   weatherIcon.classList = "card-body text-center"
-  weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`)
+  console.log(weather)
+  console.log(`https://openweathermap.org/img/wn/${weather.list[0].weather[0].icon}@2x.png`)
+  weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.list[0].weather[0].icon}@2x.png`)
   forecastEl.appendChild(weatherIcon)
 
   //temp 
   var forecastTemp = document.createElement("span")
-  temperatureEl.textContent = "Temperature: " + weather.main.temp + " °F"
-  temperatureEl.classList = "list-group-item"
+  forecastTemp.textContent = "Temperature: " + weather.list[0].main.temp + " °F"
+  forecastTemp.classList = "list-group-item"
 
   // wind
   var forecastWind = document.createElement("span")
-  windSpeedEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH"
-  windSpeedEl.classList = "list-group-item"
+  forecastWind.textContent = "Wind Speed: " + weather.list[0].wind.speed + " MPH"
+  forecastWind.classList = "list-group-item"
 
   // humidity
   var forecastHumidity = document.createElement("span")
-  humidityEl.textContent = "Humidity: " + weather.main.humidity + " %"
-  humidityEl.classList = "list-group-item"
+  forecastHumidity.textContent = "Humidity: " + weather.list[0].main.humidity + " %"
+  forecastHumidity.classList = "list-group-item"
 
   // adds elements to container 
   forecastContainerEl.appendChild(forecastTemp)
@@ -157,4 +158,4 @@ function searchHistoryHandler(event) {
 
 // HANDLERS
 searchFormEl.addEventListener("submit", formSubmitHandler);
-searchHistoryBtn.addEventListener("click", searchHistoryHandler);
+searchHistoryBtn?.addEventListener("click", searchHistoryHandler);
